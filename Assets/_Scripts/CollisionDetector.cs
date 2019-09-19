04216a5cf96fc759fa0b9b773bbcd5d726c6426c
Steven Lilley <35742090+lilleypad13@users.noreply.github.com>
2019-09-19 15:54:11 -0400

    Created small methods to detect collision and simply spawn object for testing purposes

---
+++ Assets/_Scripts/CollisionDetector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour {
+
+    #region Variables
+
+
+    #endregion
+
+    #region Unity Methods
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(gameObject.name + " has collided with" + collision.gameObject.name);
+    }
+
+
+
+    #endregion
+}
---
+++ Assets/_Scripts/SpawnTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTest : MonoBehaviour {
+
+    #region Variables
+
+
+    public GameObject testObject;
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        Instantiate(testObject);
+    }
+
+
+
+    #endregion
+}
