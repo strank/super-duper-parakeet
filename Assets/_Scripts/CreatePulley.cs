3c52e1eab382e6c006a3c08f48d32a2e52456c93
Steven Lilley <35742090+lilleypad13@users.noreply.github.com>
2019-10-06 14:15:55 -0400

    Created CreatePulley script to start system for generating pulley puzzles

---
+++ Assets/_Scripts/CreatePulley.cs
@@ -0,0 +1,352 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Range
+{
+    private float minimum;
+    private float maximum;
+
+    public float Minimum
+    {
+        get {
+            return minimum;
+        }
+
+        set
+        {
+            minimum = value;
+        }
+    }
+
+    public float Maximum
+    {
+        get
+        {
+            return maximum;
+        }
+
+        set
+        {
+            maximum = value;
+        }
+    }
+
+    public Range(float min, float max)
+    {
+        Minimum = min;
+        Maximum = max;
+    }
+}
+
+public class WeightedRange : Range
+{
+    private float weight;
+    public float Weight
+    {
+        get
+        {
+            return weight;
+        }
+
+        set
+        {
+            weight = value;
+        }
+    }
+
+    public WeightedRange(float min, float max, float p) : base(min, max)
+    {
+        Weight = p;
+    }
+}
+
+public class CreatePulley : MonoBehaviour {
+
+    #region Variables
+    [Header("Objects to Use as Pulley Ends")]
+    //public GameObject ledge;
+
+    [Header("Parameter Ranges")]
+    [Header("Rope")]
+    [Header("Length")]
+    public float minLength = 2.0f;
+    public float maxLength = 12.0f;
+    private float length;
+    private Vector3 ropeDirection;
+    private float ropeAngle;
+
+    [Header("Start Point")]
+    public Vector3 minStart = Vector3.zero;
+    public Vector3 maxStart = new Vector3(20.0f, 20.0f, 20.0f);
+    private Vector3 startPosition;
+
+    //[Header("End Point")]
+    //public Vector3 minEnd = new Vector3(1.0f, 1.0f, 1.0f);
+    //public Vector3 maxEnd = new Vector3(20.0f, 20.0f, 20.0f);
+
+    // End will be determined by system to fit the proper length
+    private Vector3 endPosition;
+
+    [Header("Wheels")]
+    [Header("Quantity")]
+    public int minWheels = 1;
+    public int maxWheels = 3;
+    private int numberOfWheels;
+
+    [Header("Position Area")]
+    public Vector3 minWheelPosition = Vector3.zero;
+    public Vector3 maxWheelPosition = new Vector3(10.0f, 10.0f, 10.0f);
+    private Vector3 wheelPosition;
+    private float minDistanceBelowRope = 3.0f;
+    private float minSeparationBetweenWheels = 2.0f;
+
+
+    // Used if setting up to create several instances at once
+    private int numberOfTests;
+    private float separationDistance;
+
+    // Trying new singleton pattern within the property itself
+    private static CreatePulley _instance;
+
+    private CreatePulley()
+    {
+        if(_instance != null)
+        {
+            return;
+        }
+
+        _instance = this;
+    }
+
+    public static CreatePulley Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                new CreatePulley();
+            }
+
+            return _instance;
+        }
+    }
+
+    public GenerationSystem generationSystem;
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        WeightedRange testRange = new WeightedRange(1.5f, 2.5f, 4.0f);
+        Debug.Log(testRange.Minimum + " also " + testRange.Maximum + " and this too " + testRange.Weight);
+
+        RandomizeStartPosition();
+        DetermineLength();
+        PositionEndPoint();
+        CreateWheelPositionRange();
+    }
+
+    /*
+     * Sets start position to a random vector3 within the min and max bounds.
+     */
+    private void RandomizeStartPosition()
+    {
+        startPosition = RandomVector3WithinBounds(minStart, maxStart);
+        Debug.Log("Randomzied pulley start position is: " + startPosition);
+    }
+
+    /*
+     * Determines a random length value based on the parameter bounds
+     */
+    private void DetermineLength()
+    {
+        length = Random.Range(minLength, maxLength);
+        Debug.Log("Length of pulley rope is: " + length);
+    }
+
+    ///*
+    // * Randomly positions the end point based on where the start point has been positioned 
+    // * and the length that should be between the start and end point.
+    // */
+    //private void PositionEndPoint()
+    //{
+    //    // Chooses a random direction, then scales this by the determined length 
+    //    // and adds it to the starting position
+    //    ropeDirection = Random.onUnitSphere;
+    //    endPosition = startPosition + ropeDirection * length;
+    //    Debug.Log("Randomized pulley end position is: " + endPosition);
+    //}
+
+    /*
+     * Randomly positions the end point based on where the start point has been positioned 
+     * and the length that should be between the start and end point.
+     * Since rope will need to fall anyway, end point is just positioned within the same xz plane as 
+     * the start point for simplicity.
+     */
+    private void PositionEndPoint()
+    {
+        // Chooses a random direction, then scales this by the determined length 
+        // and adds it to the starting position
+        ropeAngle = Random.Range(0.0f, 360.0f);
+        // Converts from degrees to radians
+        ropeAngle *= Mathf.Deg2Rad;
+
+        ropeDirection = new Vector3(
+            Mathf.Cos(ropeAngle),
+            0.0f,
+            Mathf.Sin(ropeAngle));
+
+        endPosition = startPosition + ropeDirection * length;
+        Debug.Log("Randomized pulley end position is: " + endPosition);
+    }
+
+
+    // The following method is overdone with the simplification done to PositionEndPoint, but I will 
+    // keep it for now as a reference or if the approach changes.
+    // With update, it should currently basically just draw the same line dictated by the startPosition and 
+    // endPosition, but lower in the y value.
+    /*
+     * Creates the initial range of positions for instantiating the pulley wheel
+     */
+    private Range CreateWheelPositionRange()
+    {
+        // Create the xz plane projection of the line between the start and end point
+        // Also place that line below both the start and end point
+
+        // First determine which of start and end position is lower
+        float wheelHeight = startPosition.y;
+        if (endPosition.y < startPosition.y)
+        {
+            wheelHeight = endPosition.y;
+        }
+        // Assuming separationDistance is enough lower to start the wheel height to ensure rope falls onto wheel
+        wheelHeight -= minSeparationBetweenWheels;
+
+        // Creates the start and end point for wheel positions which is a projection of the start and end positions 
+        // of the rope onto the xz plane at the determined height value
+        Vector3 wheelRangeStartPoint = new Vector3(
+            startPosition.x,
+            wheelHeight,
+            startPosition.z);
+
+        Vector3 wheelRangeEndPoint = new Vector3(
+            endPosition.x,
+            wheelHeight,
+            endPosition.z);
+
+        float lengthOfRange = Vector3.Distance(wheelRangeStartPoint, wheelRangeEndPoint);
+        Range initialWheelPositionRange = new Range(separationDistance, lengthOfRange - separationDistance);
+
+        Debug.DrawLine(startPosition, endPosition, Color.blue, 500.0f);
+        Debug.DrawLine(wheelRangeStartPoint, wheelRangeEndPoint, Color.red, 500.0f);
+        Debug.Log("Ray drawn for wheel position range.");
+        Debug.Log("Randomized wheel range start position is: " + wheelRangeStartPoint);
+        Debug.Log("Randomized wheel range end position is: " + wheelRangeEndPoint);
+
+        return initialWheelPositionRange;
+    }
+
+    /*
+     * Positions the pulley wheels along the designated range of positions
+     */
+    private void PositionWheels()
+    {
+        numberOfWheels = Random.Range(minWheels, maxWheels);
+        Vector3[] wheelPositions = new Vector3[numberOfWheels];
+
+        List<Range> possiblePositions = new List<Range>();
+        Range firstRange = CreateWheelPositionRange();
+        possiblePositions.Add(firstRange);
+
+        for (int i = 0; i < numberOfWheels; i++)
+        {
+            // Need to pick a range from the list of ranges, and then randomly select a value within that range
+
+
+
+
+
+            //Vector3 randomPositionAlongRope = startPosition + ropeDirection * Random.Range(0.0f, length);
+
+
+            // Checks newly selected point is far enough away from other determined wheel positions
+            for (int j = 0; j < i; j++)
+            {
+
+            }
+        }
+    }
+
+    /*
+     * Determines if a check value is within the buffer distance of any ranges within a list of ranges, 
+     * and edits the list of ranges accordingly.
+     */
+    private void DetermineRanges(float buffer, float check, List<Range> rangeList)
+    {
+        foreach (Range range in rangeList)
+        {
+            if(check <= (range.Minimum + buffer) && 
+                check >= (range.Maximum - buffer))
+            {
+                // Means there is no more room around this current range, so just remove it
+                rangeList.Remove(range);
+                return;
+            }
+
+            if (check <= (range.Minimum + buffer))
+            {
+                // Keeps the same amount of ranges, but one must be modified
+                // Edit range
+                range.Minimum = check + buffer;
+                return;
+            }
+
+            if (check >= (range.Maximum - buffer))
+            {
+                // Keeps the same amount of ranges, but one must be modified
+                // Edit range
+                range.Maximum = check - buffer;
+                return;
+            }
+
+            // Final check is if it is within the current range, but not near the bounds
+            if(check > range.Minimum && check < range.Maximum)
+            {
+                // Effectively splits the range into two new ranges
+                // Edit range and create new range
+
+                // The new range uses the existing range's maximum, but has a new minimum 
+                // starting after the current check value.
+                Range additionalRange = new Range(check + buffer, range.Maximum);
+
+                // The current range is edited so that it keeps the same minimum, but its maximum is now 
+                // located before the check value.
+                range.Maximum = check - buffer;
+
+                rangeList.Add(additionalRange);
+                return;
+            }
+
+        }
+    }
+
+    /*
+     * Gives a randomized Vector3 based on minimum and maximum bounds set by parameters.
+     * Vector3 will be somewhere within a bounding cube created by the min and max 
+     * bounding parameters.
+     * The bounding cubes dimensions would be (max.x - min.x, max.y - min.y, max.z - min.z)
+     */
+    private Vector3 RandomVector3WithinBounds(Vector3 min, Vector3 max)
+    {
+        Vector3 determinedVector = new Vector3(
+            Random.Range(min.x, max.x),
+            Random.Range(min.y, max.y),
+            Random.Range(min.z, max.z));
+
+        return determinedVector;
+    }
+
+    #endregion
+}
