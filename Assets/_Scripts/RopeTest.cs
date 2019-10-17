c1ee5dc7a6995b22b0967430cc528da94191bcc7
Steven Lilley <steven.b.lilley@gmail.com>
2019-10-17 16:02:12 -0400

    Just created isolated script, RopeTest, to use for debugging/testing the Rope script

---
+++ Assets/_Scripts/RopeTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeTest : MonoBehaviour {
+
+    #region Variables
+
+
+
+    #endregion
+
+
+    #region Unity Methods
+
+
+
+
+
+    #endregion
+}
