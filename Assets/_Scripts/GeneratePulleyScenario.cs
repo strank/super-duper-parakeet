27998a376f403886e580bf4687cd617beb062691
Steven Lilley <35742090+lilleypad13@users.noreply.github.com>
2019-11-15 13:32:21 -0500

    Reorganizing prefabs (and adding some new ones) with a new _Prefabs folder to take some properly tested ones out of the _Testing folder.

---
+++ Assets/_Scripts/GeneratePulleyScenario.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePulleyScenario : MonoBehaviour {
+
+    #region Variables
+
+    private ScenarioManager scenarioManager;
+    private GameObject creationManager;
+    private CreatePulley createPulley;
+
+    private PulleyType type;
+    public PulleyType Type {
+        set { type = value; }
+    }
+
+    [Header("Pulley End Physics Objects")]
+    public GameObject[] hooks; // Hook type objects that could latch onto other objects
+    public GameObject[] platforms; // Objects that could be used as a platform a player could stand on 
+    public GameObject[] massive; // Very large and heavy objects that require a lot of force to move
+    public GameObject[] genericMasses; // Assortment of objects that are mostly defined by their mass value
+
+    [Header("Wheels")]
+    public GameObject[] wheels; // Objects to use as wheels of pulley setups
+    #endregion
+
+    #region Methods
+
+    #region Initialization
+
+    private void Awake()
+    {
+        scenarioManager = ScenarioManager.Instance;
+        creationManager = scenarioManager.creationManager;
+        createPulley = creationManager.GetComponent<CreatePulley>();
+
+        // Nothing will work as intended if any of these references are not properly obtained.
+        if (scenarioManager == null || creationManager == null || createPulley == null)
+        {
+            Debug.LogWarning(name + " did not obtain a necessary reference at awake.");
+            Debug.Log("ScenarioManager is: " + scenarioManager +
+                "CreationManager is:" + creationManager +
+                "CreatePulley is:" + createPulley);
+        }
+
+        InformCreateClasses();
+
+    }
+
+    #endregion
+
+    private void InformCreateClasses()
+    {
+        switch (type)
+        {
+            case PulleyType.Hook:
+                CreateHookScenario();
+                break;
+            case PulleyType.Platform:
+                break;
+            case PulleyType.HeavyDoor:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void CreateHookScenario()
+    {
+        createPulley.SetEndObjectOptions(hooks, genericMasses);
+
+        createPulley.CreateVariedRope();
+    }
+
+    #endregion
+}
---
+++ Assets/_Scripts/ScenarioManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PuzzleType
+{
+    Seesaw,
+    Pulley,
+    SwingPlatform
+}
+
+public enum PulleyType
+{
+    Hook,
+    Platform,
+    HeavyDoor
+}
+
+[RequireComponent(typeof(GeneratePulleyScenario))]
+public class ScenarioManager : MonoBehaviour {
+
+    #region Variables
+    
+
+    public PuzzleType puzzleType;
+    public PulleyType pulleyType;
+
+    public GameObject creationManager;
+    public GeneratePulleyScenario generatePulleyScenario;
+
+    private static ScenarioManager instance;
+
+    private ScenarioManager()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+
+        instance = this;
+    }
+
+    public static ScenarioManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                new ScenarioManager();
+            }
+
+            return instance;
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    #region Initialization
+
+    private void Awake()
+    {
+        if(creationManager == null)
+        {
+            Debug.LogWarning("Scenario Manager has no Creation Manager reference.");
+        }
+        DetermineScenarioGenerator();
+    }
+
+    #endregion
+
+    private void DetermineScenarioGenerator()
+    {
+        switch (puzzleType)
+        {
+            case PuzzleType.Seesaw:
+                Debug.Log("Needs seesaw types");
+                break;
+            case PuzzleType.Pulley:
+                generatePulleyScenario.Type = pulleyType;
+                break;
+            case PuzzleType.SwingPlatform:
+                Debug.Log("Needs swing platform types");
+                break;
+            default:
+                Debug.LogWarning("This puzzle type does not exist!");
+                break;
+        }
+    }
+
+
+    #endregion
+}
