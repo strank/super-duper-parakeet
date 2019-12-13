ea888123843260e7e0dc752a6f88901f25b58779
Steven Lilley <35742090+lilleypad13@users.noreply.github.com>
2019-12-12 19:41:26 -0500

    Updated Create classes to properly be responsible for creating the specified objects in their names using methods old CreatePulley script; Also removed public parameter fields here so the ScenarioManager line would properly be responsible for those

---
+++ Assets/_Scripts/Create/CreatePulleyWheel.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePulleyWheel : MonoBehaviour {
+
+    #region Variables
+    // Received from Scenario class
+    // Rope Information
+    private Vector3 ropeDirection;
+    private float ropeAngle;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+
+    // Wheels
+    private GameObject[] pulleyWheelOptions;
+    private int minWheels;
+    private int maxWheels;
+
+    // Other
+    private float minSeparationBetweenWheels = 2.0f;
+    private float distanceFromEnds = 2.0f;
+    private bool useDefaultDistanceBelowRopeForWheels = true;
+    private float distanceBelowRopeToSpawnWheels = 2.0f;
+
+    // Determined within class itself
+    private int numberOfWheels;
+    private Vector3[] wheelPositions;
+    private Vector3 wheelRangeStartPoint;
+    private Vector3 wheelRangeEndPoint;
+
+
+    #endregion
+
+    #region Unity Methods
+
+    public void BuildPulleyWheel(Vector3 _startPosition, Vector3 _endPosition, bool _useDefaultDistanceBelowRopeForWheels, 
+        float _distanceBelowRopeToSpawnWheels, float _minSeparationBetweenWheels, float _ropeAngle, Vector3 _ropeDirection, 
+        float _distanceFromEnds)
+    {
+        startPosition = _startPosition;
+        endPosition = _endPosition;
+        useDefaultDistanceBelowRopeForWheels = _useDefaultDistanceBelowRopeForWheels;
+        distanceBelowRopeToSpawnWheels = _distanceBelowRopeToSpawnWheels;
+        minSeparationBetweenWheels = _minSeparationBetweenWheels;
+        ropeAngle = _ropeAngle;
+        ropeDirection = _ropeDirection;
+        distanceFromEnds = _distanceFromEnds;
+
+        CreateWheelPositionRange();
+        PositionWheels();
+        GenerateWheels();
+    }
+
+    // The following method is overdone with the simplification done to PositionEndPoint, but I will 
+    // keep it for now as a reference or if the approach changes.
+    // With update, it should currently basically just draw the same line dictated by the startPosition and 
+    // endPosition, but lower in the y value.
+    /*
+     * Creates the initial range of positions for instantiating the pulley wheel
+     */
+    private WeightedRange CreateWheelPositionRange()
+    {
+        // Create the xz plane projection of the line between the start and end point
+        // Also place that line below both the start and end point
+
+        // First determine which of start and end position is lower
+        float wheelHeight = startPosition.y;
+        if (endPosition.y < startPosition.y)
+        {
+            wheelHeight = endPosition.y;
+        }
+        // Assuming separationDistance is lower enough to start the wheel height to ensure rope falls onto wheel
+        // Can use another value if that assumption gives poor results
+        if (useDefaultDistanceBelowRopeForWheels)
+            wheelHeight -= minSeparationBetweenWheels;
+        else
+            wheelHeight -= distanceBelowRopeToSpawnWheels;
+
+        // Creates the start and end point for wheel positions which is a projection of the start and end positions 
+        // of the rope onto the xz plane at the determined height value
+        wheelRangeStartPoint = new Vector3(
+            startPosition.x,
+            wheelHeight,
+            startPosition.z);
+
+        wheelRangeEndPoint = new Vector3(
+            endPosition.x,
+            wheelHeight,
+            endPosition.z);
+
+        float lengthOfRange = Vector3.Distance(wheelRangeStartPoint, wheelRangeEndPoint);
+        WeightedRange initialWheelPositionRange = new WeightedRange(distanceFromEnds, lengthOfRange - distanceFromEnds, 1.0f);
+
+        Debug.DrawLine(startPosition, endPosition, Color.blue, 500.0f);
+        Debug.DrawLine(wheelRangeStartPoint, wheelRangeEndPoint, Color.red, 500.0f);
+        Debug.Log("Ray drawn for wheel position range.");
+        Debug.Log("Randomized wheel range start position is: " + wheelRangeStartPoint);
+        Debug.Log("Randomized wheel range end position is: " + wheelRangeEndPoint);
+
+        return initialWheelPositionRange;
+    }
+
+    /*
+     * Positions the pulley wheels along the designated range of positions
+     */
+    private void PositionWheels()
+    {
+        numberOfWheels = Random.Range(minWheels, maxWheels + 1); // Need the plus 1 because the maximum of the range is exclusive
+        wheelPositions = new Vector3[numberOfWheels];
+
+        List<WeightedRange> positionRanges = new List<WeightedRange>();
+        WeightedRange firstRange = CreateWheelPositionRange();
+        positionRanges.Add(firstRange);
+
+        for (int i = 0; i < numberOfWheels; i++)
+        {
+            // Need to pick a range from the list of ranges, and then randomly select a value within that range
+            float rangePicker = Random.Range(0.0f, 1.0f);
+            float weightCheck = 0.0f;
+            WeightedRange selectedRange;
+            int rangeCounter = 0;
+
+            while (weightCheck < rangePicker)
+            {
+                weightCheck += positionRanges[rangeCounter].Weight;
+                rangeCounter++;
+            }
+
+            // Loop completes once it finds the proper range, but still increments
+            // the counter one last time, so the selectedRange subtracts 1 to make up for this.
+            selectedRange = positionRanges[rangeCounter - 1];
+
+            // Selects a random value from the chosen range and uses this to set this wheel position
+            float selectedValue = Random.Range(selectedRange.Minimum, selectedRange.Maximum);
+            wheelPositions[i] = wheelRangeStartPoint + ropeDirection * selectedValue;
+
+            // Uses the randomly selected value to recalculate the possible position ranges
+            DetermineRanges(minSeparationBetweenWheels, selectedValue, positionRanges);
+            ReweightRangeList(positionRanges);
+        }
+    }
+
+    /*
+     * Determines if a check value is within the buffer distance of any ranges within a list of ranges, 
+     * and edits the list of ranges accordingly.
+     */
+    private void DetermineRanges(float buffer, float check, List<WeightedRange> rangeList)
+    {
+        foreach (WeightedRange range in rangeList)
+        {
+            if (check <= (range.Minimum + buffer) &&
+                check >= (range.Maximum - buffer))
+            {
+                // Means there is no more room around this current range, so just remove it
+                rangeList.Remove(range);
+                return;
+            }
+
+            if (check <= (range.Minimum + buffer))
+            {
+                // Keeps the same amount of ranges, but one must be modified
+                // Edit range
+                range.Minimum = check + buffer;
+                return;
+            }
+
+            if (check >= (range.Maximum - buffer))
+            {
+                // Keeps the same amount of ranges, but one must be modified
+                // Edit range
+                range.Maximum = check - buffer;
+                return;
+            }
+
+            // Final check is if it is within the current range, but not near the bounds
+            if (check > range.Minimum && check < range.Maximum)
+            {
+                // Effectively splits the range into two new ranges
+                // Edit range and create new range
+
+                // The new range uses the existing range's maximum, but has a new minimum 
+                // starting after the current check value.
+                WeightedRange additionalRange = new WeightedRange(check + buffer, range.Maximum, 0.0f);
+
+                // The current range is edited so that it keeps the same minimum, but its maximum is now 
+                // located before the check value.
+                range.Maximum = check - buffer;
+
+                rangeList.Add(additionalRange);
+                return;
+            }
+
+        }
+    }
+
+    /*
+     * Takes a list of weighted ranges and recalculates their weights so that each individual weighted range 
+     * has a weight value which corresponds directly to the proportion of the total range of all weighted ranges 
+     * that that specific weighted range covers.
+     */
+    private void ReweightRangeList(List<WeightedRange> rangeList)
+    {
+        float rangeSum = 0.0f;
+
+        foreach (WeightedRange range in rangeList)
+        {
+            rangeSum += (range.Maximum - range.Minimum);
+        }
+
+        foreach (WeightedRange range in rangeList)
+        {
+            range.Weight = (range.Maximum - range.Minimum) / rangeSum;
+        }
+    }
+
+    /*
+     * Instantiates a number of pulley wheels equal to the number of locations within the 
+     * wheelPositions array at the location specified by the wheelPositions array.
+     */
+    private void GenerateWheels()
+    {
+        // Determine the rotation needed for the wheels to line up with the rope
+        ropeAngle *= -1 * Mathf.Rad2Deg; // Needs the -1 so it properly lines up the Unity rotation
+        Debug.Log("Wheel generator ropeAngle is: " + ropeAngle);
+
+        foreach (Vector3 v in wheelPositions)
+        {
+            int randomWheelIndex = Random.Range(0, pulleyWheelOptions.Length);
+            GameObject pulleyWheel = pulleyWheelOptions[randomWheelIndex];
+            Instantiate(pulleyWheel, v, Quaternion.Euler(0f, ropeAngle, 0f));
+        }
+    }
+
+    #endregion
+}
--- Assets/_Scripts/Create/CreateRope.cs
+++ Assets/_Scripts/Create/CreateRope.cs
@@ -6,21 +6,24 @@ using HumanAPI;
 public class CreateRope : MonoBehaviour {
 
     #region Variables
-    public GameObject ropePrefab;
-    private Rope rope;
+    private Vector3 startPosition;
+    private Vector3 minStart;
+    private Vector3 maxStart;
 
-    public GameObject startPrefab;
-    public GameObject endPrefab;
+    private float minLength;
+    private float maxLength;
 
-    private GameObject startObject;
-    private GameObject endObject;
-    private GameObject generatedRope;
+    private GameObject[] startPulleyMassOptions;
+    private GameObject[] endPulleyMassOptions;
 
-    public Transform ropePosition;
-    public Transform ropeStart;
-    public Transform ropeEnd;
+    // Determined within this Create class
+    private float length;
+    private float ropeAngle;
+    private Vector3 ropeDirection;
+    private Vector3 endPosition;
 
-    public float offsetX = 1.0f;
+    // Always set for this Create class
+    public GameObject pulleyPrefab;
 
     // Important Rope variables
     /*
@@ -40,39 +43,157 @@ public class CreateRope : MonoBehaviour {
     #endregion
 
     #region Unity Methods
-
-    private void Start()
+    
+    /*
+     * Public method to allow outside classes to use this class to build a rope
+     */
+    public void BuildRope(Vector3 _minStart, Vector3 _maxStart, float _minLength, float _maxLength, 
+        GameObject[] _startPulleyMassOptions, GameObject[] _endPulleyMassOptions)
     {
+        minStart = _minStart;
+        maxStart = _maxStart;
+        minLength = _minLength;
+        maxLength = _maxLength;
+        startPulleyMassOptions = _startPulleyMassOptions;
+        endPulleyMassOptions = _endPulleyMassOptions;
 
-        CreateObjects();
-
-        SetRopeParameters();
+        RandomizeStartPosition();
+        DetermineLength();
+        PositionEndPoint();
+        PlaceObjectsOnRopeEnds();
     }
 
-    private void CreateObjects()
+    /*
+     * Sets start position to a random vector3 within the min and max bounds.
+     */
+    private void RandomizeStartPosition()
     {
-        generatedRope = (GameObject)Instantiate(ropePrefab, ropePosition.position, Quaternion.identity);
-        rope = generatedRope.GetComponent<PulleySetup>().rope;
-
-
-        Vector3 startObjectPosition = ropeStart.position - new Vector3(offsetX, 0.0f, 0.0f);
-        Vector3 endObjectPosition = ropeEnd.position + new Vector3(offsetX, 0.0f, 0.0f);
-
-        startObject = (GameObject)Instantiate(startPrefab, startObjectPosition, Quaternion.identity, generatedRope.transform);
-        endObject = (GameObject)Instantiate(endPrefab, endObjectPosition, Quaternion.identity, generatedRope.transform);
+        startPosition = RandomGeneration.RandomVector3WithinBounds(minStart, maxStart);
+        Debug.Log("Randomzied pulley start position is: " + startPosition);
     }
 
-    private void SetRopeParameters()
+    /*
+     * Determines a random length value based on the parameter bounds
+     */
+    private void DetermineLength()
+    {
+        length = Random.Range(minLength, maxLength);
+        Debug.Log("Length of pulley rope is: " + length);
+    }
+
+    /*
+     * Randomly positions the end point based on where the start point has been positioned 
+     * and the length that should be between the start and end point.
+     * Since rope will need to fall anyway, end point is just positioned within the same xz plane as 
+     * the start point for simplicity.
+     */
+    private void PositionEndPoint()
+    {
+        // Chooses a random direction, then scales this by the determined length 
+        // and adds it to the starting position
+        ropeAngle = Random.Range(0.0f, 360.0f);
+        // Converts from degrees to radians
+        ropeAngle *= Mathf.Deg2Rad;
+
+        ropeDirection = new Vector3(
+            Mathf.Cos(ropeAngle),
+            0.0f,
+            Mathf.Sin(ropeAngle));
+
+        endPosition = startPosition + ropeDirection * length;
+        Debug.Log("Randomized pulley end position is: " + endPosition);
+    }
+
+    /*
+     * Instantiates the base prefab used for the pulley system, as well as the objects determined to be the 
+     * ones at the end(s) of the pulley system.
+     * Also connects these instantiated objects to the rope of the base prefab to solidify the entire 
+     * pulley system.
+     */
+    private void PlaceObjectsOnRopeEnds()
+    {
+        Vector3 TESTPOSITION = new Vector3(0f, 10f, 0f);
+        Vector3 OFFSET = new Vector3(0.5f, 0f, 0f);
+        GameObject firstObject = null;
+        GameObject secondObject = null;
+
+        GameObject quickPulley = (GameObject)Instantiate(pulleyPrefab, TESTPOSITION, Quaternion.identity);
+        PulleySetup pulleySetup = quickPulley.GetComponent<PulleySetup>();
+        Rope pulleyRope = pulleySetup.rope;
+
+        // Moves the internal pulley prefab empty gameobject transforms to the proper position.
+        // Needed to hold useful positional data as well as provide transform data for rope handles
+        pulleySetup.ropeStart.transform.position = startPosition;
+        pulleySetup.ropeEnd.transform.position = endPosition;
+        Debug.Log("Rope start transform position moved to : " + startPosition);
+        Debug.Log("Rope end transform position moved to : " + endPosition);
+
+        // Instantiate the objects which make up the pulley end masses
+        Vector3 firstObjectPosition = startPosition - OFFSET;
+        Vector3 secondObjectPosition = endPosition + OFFSET;
+        if (startPulleyMassOptions.Length > 0)
+        {
+            GameObject startPulleyMass = startPulleyMassOptions[Random.Range(0, startPulleyMassOptions.Length)];
+            firstObject = (GameObject)Instantiate(startPulleyMass, firstObjectPosition, Quaternion.identity, quickPulley.transform);
+        }
+        if (endPulleyMassOptions.Length > 0)
+        {
+            GameObject endPulleyMass = endPulleyMassOptions[Random.Range(0, endPulleyMassOptions.Length)];
+            secondObject = (GameObject)Instantiate(endPulleyMass, secondObjectPosition, Quaternion.identity, quickPulley.transform);
+        }
+
+        SetRopeParameters(pulleyRope, pulleySetup.ropeStart.transform, pulleySetup.ropeEnd.transform, firstObject, secondObject);
+    }
+
+    /*
+     * Effectively connects the startObject and the endObject to the rope object that is 
+     * passed in all into a single rope system.
+     */
+    private void SetRopeParameters(Rope rope, Transform ropeStart, Transform ropeEnd,
+        GameObject startObject, GameObject endObject)
     {
         rope.handles[0] = ropeStart;
         rope.handles[1] = ropeEnd;
 
-        rope.startBody = startObject.GetComponent<Rigidbody>();
-        rope.endBody = endObject.GetComponent<Rigidbody>();
+        if (startObject != null)
+            rope.startBody = startObject.GetComponentInChildren<Rigidbody>();
+
+        if (endObject != null)
+            rope.endBody = endObject.GetComponentInChildren<Rigidbody>();
 
         rope.gameObject.SetActive(true);
     }
 
+    //private void Start()
+    //{
 
+    //    CreateObjects();
+
+    //    SetRopeParameters();
+    //}
+
+    //private void CreateObjects()
+    //{
+    //    generatedRope = (GameObject)Instantiate(ropePrefab, ropePosition.position, Quaternion.identity);
+    //    rope = generatedRope.GetComponent<PulleySetup>().rope;
+
+
+    //    Vector3 startObjectPosition = ropeStart.position - new Vector3(offsetX, 0.0f, 0.0f);
+    //    Vector3 endObjectPosition = ropeEnd.position + new Vector3(offsetX, 0.0f, 0.0f);
+
+    //    startObject = (GameObject)Instantiate(startPrefab, startObjectPosition, Quaternion.identity, generatedRope.transform);
+    //    endObject = (GameObject)Instantiate(endPrefab, endObjectPosition, Quaternion.identity, generatedRope.transform);
+    //}
+
+    //private void SetRopeParameters()
+    //{
+    //    rope.handles[0] = ropeStart;
+    //    rope.handles[1] = ropeEnd;
+
+    //    rope.startBody = startObject.GetComponent<Rigidbody>();
+    //    rope.endBody = endObject.GetComponent<Rigidbody>();
+
+    //    rope.gameObject.SetActive(true);
+    //}
     #endregion
 }
