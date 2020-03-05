02f1291126595cbe636583898d79d74eb8782aa3
Steven Lilley <steven.b.lilley@gmail.com>
2020-03-05 01:21:46 -0500

    Created a full hierarchy of classes as the skeleton for the Lever scenarios. This is the high level Lever manager as well as a sample of the lowest level class

---
+++ Assets/_Scripts/Scenario/LeverOneScenario.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverOneScenario : LeverScenarioManager {
+
+    //TODO: NEEDS FIXED, VERY BROKEN CURRENTLY
+
+    public override void BuildScenario()
+    {
+        CreateSeesaw();
+        CreateObstacle();
+    }
+
+    private void CreateSeesaw()
+    {
+        CreateSeesaw seesaw = creationManager.GetComponent<CreateSeesaw>();
+
+        seesaw.BuildSeesaw(minStart, maxStart, minLength, maxLength, minWidth, maxWidth, minHeight, maxHeight,
+            scenarioParent, rng);
+
+        // TODO: Determine which parameters to pass on for obstacle information
+
+    }
+}
---
+++ Assets/_Scripts/Scenario/LeverScenarioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HumanAPI;
+
+public abstract class LeverScenarioManager : ScenarioManager {
+
+    #region Variables
+    [Header("Parameter Ranges")]
+    [Header("Lever Dimensions")]
+    public float minLength = 2.0f;
+    public float maxLength = 10.0f;
+    public float minWidth = 2.0f;
+    public float maxWidth = 10.0f;
+    public float minHeight = 2.0f;
+    public float maxHeight = 8.0f;
+
+    [Header("Other")]
+    public Vector3 minStart = new Vector3(0.0f, 0.0f, 0.0f);
+    public Vector3 maxStart = new Vector3(5.0f, 5.0f, 5.0f);
+
+    #endregion
+
+    #region Unity Methods
+
+    protected void CreateObstacle()
+    {
+        CreateObstacleTerrain obstacle = creationManager.GetComponent<CreateObstacleTerrain>();
+
+        // Use height instead of maxHeight so obstacle is never greater than ramp size
+        obstacle.BuildObstacle(minStart, maxStart, minLength, maxLength, minWidth, maxWidth, minHeight, maxHeight,
+            scenarioParent, rng);
+    }
+
+    #endregion
+}
