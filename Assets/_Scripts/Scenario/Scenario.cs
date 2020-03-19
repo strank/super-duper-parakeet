138fc1ceaf7223e4b1eb0a1089677a6d36cb3fca
Steven Lilley <steven.b.lilley@gmail.com>
2020-03-19 19:40:19 -0400

    May need a new separate Scenario object for system

---
+++ Assets/_Scripts/Scenario/Scenario.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenario : MonoBehaviour {
+
+    public System.Random RNG { get { return rng; } }
+    private System.Random rng;
+
+    public GameObject ScenarioParent { get { return scenarioParent; } }
+    private GameObject scenarioParent;
+
+    public Vector3 CentralLocation { get { return centralLocation; } }
+    private Vector3 centralLocation;
+
+    private List<Skill> skillSet;
+
+    public Scenario(System.Random _rng, GameObject _scenarioParent, Vector3 _centralLocation)
+    {
+        rng = _rng;
+        scenarioParent = _scenarioParent;
+        centralLocation = _centralLocation;
+        skillSet = new List<Skill>();
+    }
+
+    public void RecordSkill(Skill skill)
+    {
+        skillSet.Add(skill);
+    }
+}
