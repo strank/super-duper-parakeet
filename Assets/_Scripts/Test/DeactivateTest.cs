738af3f56405baffab7fd35db85a92cff5f1e7a1
Steven Lilley <steven.b.lilley@gmail.com>
2020-04-02 18:01:11 -0400

    Quick testing script to see what objects did when deactivated at run time

---
+++ Assets/_Scripts/Test/DeactivateTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class DeactivateTest : MonoBehaviour {
+
+	[SerializeField]private float duration = 3.0f;
+
+	private void Start()
+	{
+		StartCoroutine(DeactivateAfterTime());
+	}
+
+	private IEnumerator DeactivateAfterTime()
+	{
+		yield return new WaitForSeconds(duration);
+		this.gameObject.SetActive(false);
+	}
+}
