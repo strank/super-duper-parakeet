0f54cba69bb436b2aa65135a586076a2f0e9ab5e
Stefan Rank <strank@strank.info>
2020-04-23 14:30:16 -0400

    Move vigemclient.dll to project root, it is found at runtime there. Proof of concept simulated input.

--- Assets/_Scripts/MLAgents/HFFAgent.cs
+++ Assets/_Scripts/MLAgents/HFFAgent.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HFFAgent : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+public class HFFAgent : MonoBehaviour
+{
+    private HFFSimInput hffInput;
+
+    public void Awake()
+    {
+        hffInput = new HFFSimInput();
+        hffInput.ConnectController();
+        Debug.Log("Simulated Controller connected.");
+    }
+
+    public void OnApplicationQuit() {
+        hffInput.DisconnectController();
+        Debug.Log("Simulated Controller disconnected.");
+    }
+
+    public void Start()
+    {
+        Debug.Log("starting input simulation!");
+        StartCoroutine(this.SimulateInput());
+    }
+
+    private IEnumerator SimulateInput() {
+        yield return new WaitForSeconds(9);
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            Debug.Log("simulating input start");
+            hffInput.Look(32000, 32000);
+            yield return new WaitForSeconds(1f);
+            Debug.Log("simulating input end");
+            hffInput.Look(0, 0);
+        }
+    }
 }
---
+++ Assets/_Scripts/MLAgents/HFFSimInput.cs
@@ -0,0 +1,81 @@
+﻿using Nefarius.ViGEm.Client;
+using Nefarius.ViGEm.Client.Targets;
+using Nefarius.ViGEm.Client.Targets.Xbox360;
+
+/// <summary>
+/// Simulate keyboard and mouse input by going through the native Windows OS layer.
+/// Independent of Unity, and Windows-only.
+/// This is now based on the ViGEm bus tool, so the corresponding system driver needs
+/// to be installed. (Installer is in repository.)
+/// This approach still requires the editor's game window to be in focus for it to work!
+/// So using it for Agent training means the user is blocked from using the machine
+/// for anything else.
+/// TODO: can we "capture" the gamepad, so it sends input even if window is inactive
+/// </summary>
+
+// HISTORY:
+// I previously tried to use Unity's experimental new InputSystem to queue events
+// but those events didn't register with the HFF character.
+// Then I successfully used the approach found here:
+// https://answers.unity.com/questions/564664/how-i-can-move-mouse-cursor-without-mouse-but-with.html
+// then used the package InputSimulator https://github.com/michaelnoonan/inputsimulator/
+// which is similar but uses the modern API not a deprecated one and adds keyboard input.
+// However, both of those are problematic as the input isn't restricted to the current process or window,
+// so using this could mean accidentally clicking anything on the screen!
+// This could be worked around by capturing the mouse cursor, but simulating a gamepad seemed safer.
+
+
+public class HFFSimInput {
+    static ViGEmClient vigemClient = new ViGEmClient();
+    private Xbox360Controller xboxController = new Xbox360Controller(vigemClient);
+
+    public void ConnectController() {
+        xboxController.Connect();
+    }
+
+    public void DisconnectController() {
+        xboxController.Disconnect();
+        vigemClient.Dispose();
+    }
+
+    // Functions corresponding to the Input Manager mapping in Unity:
+
+    // Jump is Button A
+    public void JumpStart() {
+        xboxController.SetButtonState(Xbox360Button.A, true);
+    }
+    public void JumpEnd() {
+        xboxController.SetButtonState(Xbox360Button.A, false);
+    }
+
+    // LeftGrab is LT
+    public void LeftGrabStart() {
+        xboxController.SetSliderValue(Xbox360Slider.LeftTrigger, 0xFF);
+    }
+    public void LeftGrabEnd() {
+        xboxController.SetSliderValue(Xbox360Slider.LeftTrigger, 0x00);
+    }
+
+    // RightGrab is RT
+    public void RightGrabStart() {
+        xboxController.SetSliderValue(Xbox360Slider.RightTrigger, 0xFF);
+    }
+    public void RightGrabEnd() {
+        xboxController.SetSliderValue(Xbox360Slider.RightTrigger, 0x00);
+    }
+
+    // LookHorizontal is right stick
+    // LookVertical is right stick
+    public void Look(short whereX, short whereY) {
+        xboxController.SetAxisValue(Xbox360Axis.RightThumbX, whereX);
+        xboxController.SetAxisValue(Xbox360Axis.RightThumbY, whereY);
+    }
+
+    // WalkHorizontal is left stick
+    // WalkVertical is left stick
+    public void Walk(short whereX, short whereY) {
+        xboxController.SetAxisValue(Xbox360Axis.LeftThumbX, whereX);
+        xboxController.SetAxisValue(Xbox360Axis.LeftThumbY, whereY);
+    }
+
+}
